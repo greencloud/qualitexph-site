47191a531730ffe17801f766cc947aec25377854
Greencloud <Greencloud>
2026-01-01 17:55:29 +0800

    implement some security headers

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,43 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  // Prevent clickjacking
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+
+  // Prevent MIME sniffing
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+
+  // Control referrer information
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+
+  // Lock down powerful browser features
+  {
+    key: 'Permissions-Policy',
+    value: [
+      'accelerometer=()',
+      'autoplay=()',
+      'camera=()',
+      'clipboard-read=()',
+      'clipboard-write=()',
+      'geolocation=()',
+      'gyroscope=()',
+      'magnetometer=()',
+      'microphone=()',
+      'payment=()',
+      'usb=()',
+    ].join(', '),
+  },
+]
+
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
@@ -10,7 +48,15 @@ const nextConfig: NextConfig = {
         pathname: '/**'
       }
     ]
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
